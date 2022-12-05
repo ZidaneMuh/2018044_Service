7a0f4443b89f819a82adc868aae42347dd5c7d82
ZidaneMuh <zrusminto44@gmail.com>
2022-12-05 23:31:39 +0700

    Muhammad Zidan Rusminto

---
+++ app/src/main/java/com/example/a2018044_service/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a2018044_service;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button buttonStart, buttonStop;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+    }
+    public void onClick(View src) {
+        switch (src.getId()) {
+            case R.id.buttonStart:
+
+                startService(new Intent(this, MyService.class));
+                break;
+            case R.id.buttonStop:
+                stopService(new Intent(this, MyService.class));
+                break;
+        }
+    }
+}
